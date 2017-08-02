56b68f36c4aa5e470221bdfa76fdc2ddc7c7d6c3
Rakesh <rakeshjack7@gmail.com>
2017-08-02 17:15:59 +0530

    Navigation fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +11,8 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+    @Output() featureSelected = new EventEmitter<string>();
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }

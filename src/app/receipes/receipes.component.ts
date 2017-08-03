c8cdc13cfaf02481789daddfffa490cb46555875
Rakesh <rakeshjack7@gmail.com>
2017-08-03 16:43:38 +0530

    Recipe commit

--- src/app/receipes/receipes.component.ts
+++ src/app/receipes/receipes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from './recipe.model';
 @Component({
   selector: 'app-receipes',
   templateUrl: './receipes.component.html',
   styleUrls: ['./receipes.component.css']
 })
 export class ReceipesComponent implements OnInit {
+    selectedRecipe: Recipe;
 
   constructor() { }
 
--- src/app/receipes/recipe-list/recipe-list.component.ts
+++ src/app/receipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +6,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[]=[new Recipe('A Test Recipe','This is a simple Test','https://get.pxhere.com/photo/plant-fruit-sweet-glass-summer-food-produce-drink-lychee-dessert-cake-cocktail-grapefruit-mojito-jelly-exotic-citrus-verne-flowering-plant-suites-france-confectionery-land-plant-mai-tai-mint-julep-caipirinha-1020887.jpg'),
     new Recipe('Coffe Mug','This is Butter Scotch','https://get.pxhere.com/photo/keyboard-white-tea-morning-glass-cup-green-ceramic-drink-lighting-coffee-cup-bed-3204.jpg')];
   constructor() { }
 
   ngOnInit() {
+  }
+  onRecipeSelected(recipe: Recipe) {
+      console.log(recipe);
+      alert('I am here');
+      this.recipeWasSelected.emit(recipe);
   }
 
 }

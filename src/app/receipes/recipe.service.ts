0f2206dc50100466c6e377d5bc1a94ed929a88ce
Rakesh <rakeshjack7@gmail.com>
2017-08-04 19:23:49 +0530

    Shopping list services with the commenents are done

--- src/app/receipes/recipe.service.ts
+++ src/app/receipes/recipe.service.ts
@@ -1,6 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter,Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[]=[
@@ -16,9 +18,14 @@ export class RecipeService {
     [
         new Ingredient('Milk',1),
         new Ingredient('Hot Water',20)
-    ])];
-    
+    ])]; 
+    constructor(private slService: ShoppingListService) {
+        
+    }
     getRecipes() {
         return this.recipes.slice();
+    }
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);        
     }
 }

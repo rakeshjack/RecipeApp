cd217cab8f6c001c8a57fbd6f53c5654b9035261
Rakesh <rakeshjack7@gmail.com>
2017-08-04 18:43:47 +0530

    Working on Ingredients in shopping list

--- src/app/receipes/recipe.model.ts
+++ src/app/receipes/recipe.model.ts
@@ -1,10 +1,13 @@
+import {Ingredient} from '../shared/ingredient.model';
 export class Recipe {
     public name: string;
-    public description:string;
-    public imagepath:string;
-    constructor(name:string,desc:string,imagepath:string) {
-        this.name=name;
-        this.description=desc;
-        this.imagepath=imagepath;
+    public description: string;
+    public imagepath: string;
+    public ingredients: Ingredient[];
+    constructor(name: string, desc: string, imagepath: string,ingredients: Ingredient[]) {
+        this.name = name;
+        this.description = desc;
+        this.imagepath = imagepath;
+        this.ingredients=ingredients;
     }
 }

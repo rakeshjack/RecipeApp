f9e597ca25579564db99cf61e3934d174f2b3b1d
Rakesh <rakeshjack7@gmail.com>
2017-08-08 13:54:43 +0530

    WOrking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
-import { Routes,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { ReceipesComponent } from "./receipes/receipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./receipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./receipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./receipes/recipe-edit/recipe-edit.component";
 
 
-const appRoutes: Routes=[
-        {path: '',redirectTo:'/recipes',pathMatch:'full'},
-        {path: 'recipes',component:ReceipesComponent, children:[
-            {path: '', component: RecipeStartComponent},
-            {path: ':id',component: RecipeDetailComponent}
-        ] },
-        {path: 'shopping-list',component: ShoppingListComponent }
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', component: ReceipesComponent, children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent }
+
+        ]
+    },
+    { path: 'shopping-list', component: ShoppingListComponent }
 ];
 @NgModule({
-    imports:[RouterModule.forRoot(appRoutes)],
-    exports:[RouterModule]
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
- 
+
 }
--- src/app/receipes/recipe-list/recipe-list.component.ts
+++ src/app/receipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import { Router, ActivatedRoute } from "@angular/router";
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -9,13 +10,16 @@ import {RecipeService} from '../recipe.service';
 export class RecipeListComponent implements OnInit {
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[]; 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+        private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
       this.recipes = this.recipeService.getRecipes();
   }
-  onRecipeSelected(recipe: Recipe) {
-      this.recipeWasSelected.emit(recipe);
-  }
+
+    onNewRecipe() {
+        this.router.navigate(['new'],{relativeTo: this.route});
+    }
 
 }
